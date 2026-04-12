d0998870d6791ee018650e6797c8b381769241f6
Digitaliya Uruguaym <digitaliyauruguay@gmail.com>
2026-04-12 16:36:15 -0300

    v5 - color pattern fix

--- app/theme-provider.jsx
+++ app/theme-provider.jsx
@@ -1,14 +1,13 @@
 "use client";
-import { useEffect } from "react";
 import { siteConfig } from "./config/site";
 
 export default function ThemeProvider({ children }) {
-  useEffect(() => {
+  if (typeof window !== "undefined") {
     const root = document.documentElement;
 
     root.style.setProperty("--primary", siteConfig.theme.primary);
     root.style.setProperty("--primary-hover", siteConfig.theme.primaryHover);
-  }, []);
+  }
 
   return children;
 }
