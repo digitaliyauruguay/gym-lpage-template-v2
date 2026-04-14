cbbe593f961bf2d24c1d6f803a82001cf4dfa927
Digitaliya Uruguaym <digitaliyauruguay@gmail.com>
2026-04-14 14:28:38 -0300

    v2.0 - Env variables added

--- app/lib/airtable.js
+++ app/lib/airtable.js
@@ -1,3 +1,3 @@
-export const AIRTABLE_BASE_ID = "app1wBrQU93YK9j7Z";
-export const AIRTABLE_API_KEY = "patPqaO4ejnwQ6e9H.93fab5b1a31e7811245ff58bb52b3f7e15fff87b1af4a1383e15827305a63b81";
-export const AIRTABLE_TABLE_NAME = "leads";
+export const AIRTABLE_BASE_ID = process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID;
+export const AIRTABLE_API_KEY = process.env.NEXT_PUBLIC_AIRTABLE_API_KEY;
+export const AIRTABLE_TABLE_NAME = process.env.NEXT_PUBLIC_AIRTABLE_TABLE_NAME;
