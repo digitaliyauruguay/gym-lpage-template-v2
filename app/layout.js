7d8d2aba72b20f4be11c913e19e113815ba5e2cf
Digitaliya Uruguaym <digitaliyauruguay@gmail.com>
2026-04-16 17:36:49 -0300

    v11 - airtable + auto + google tag

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Script from "next/script"; // 👈 IMPORTANTE
 import ThemeProvider from "./theme-provider";
 import { siteConfig } from "./config/site";
 
@@ -15,6 +16,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-KMHHVYQSDE"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KMHHVYQSDE');
+          `}
+        </Script>
+      </head>
+
       <body className={inter.className}>
         <ThemeProvider>
           {children}
